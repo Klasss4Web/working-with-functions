ee312bd433ad6cd0870c9e18ade2932a1096546f
OCHADE EMMANUEL <eochade16@gmail.com>
2021-03-11 10:00:22 +0100

    Added the assigment on conditionals to this repo

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/********************ASSIGNMENT ONE**********************/
+
 //Create a function to perform mathematical operations based on the third argument
 
 function returnOperationType (num1, num2, operation) {
@@ -26,3 +28,48 @@ console.log(returnOperationType(100, 20, 'sum'));
 console.log(returnOperationType(100, 20, 'multiplication'));
 console.log(returnOperationType(100, 20, 'division'));
 console.log(returnOperationType(100, 20, 'null'));
+
+
+
+
+/*****************ASSIGNMENT TWO***********************/
+/*****************CONDITIONALS************************/
+
+
+//Conditionals
+//Students Grades
+const jamesGrades = 100;
+const peterGrades = 69;
+const paulGrades = 50;
+
+//Comparing Peter's grade to Paul's and Jame's
+if(peterGrades > paulGrades && peterGrades > jamesGrades) 
+{
+  console.log("Peter is the best!");
+}
+//Checking for Peter's, James and Paul's overall position
+else if(peterGrades > paulGrades && peterGrades < jamesGrades)
+{
+  console.log("James is the best but Peter is beter than Paul");
+}
+//Checking if Peter came out last in the class
+else{
+  console.log("Peter did worse this year");
+}
+
+//Using the Equality sign 
+const specialNum = "100";
+const numeral = 100;
+
+if(specialNum == numeral) {
+  console.log("Both are equivalent numbers")
+}else {
+  console.log("Both are not equivalent numbers")
+}
+
+//Using the Strict Equivalence
+if(specialNum === numeral) {
+  console.log("Both are exact numbers")
+}else {
+  console.log("We are not strictly equal");
+}
